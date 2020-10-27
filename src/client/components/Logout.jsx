881b58ca9254173fbb581770a05eba9f34e22994
Oluwaseyi Aromokeye <aromokeyes2@gmail.com>
2020-10-27 11:32:33 +0100

    chore: refactor components

--- src/client/components/Logout.jsx
+++ src/client/components/Logout.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Logout = ({ handleSignoutClick, setCalendars, setEvents, setCategories }) => {
-  const onSignOutClick = React.useCallback(
-    (event) => {
-      event.preventDefault();
-      handleSignoutClick();
-      setCalendars([]);
-      setEvents([]);
-      setCategories([]);
-    },
-    [handleSignoutClick, setCalendars, setEvents, setCategories],
-  );
+  const onSignOutClick = (event) => {
+    event.preventDefault();
+    handleSignoutClick();
+    setCalendars([]);
+    setEvents([]);
+    setCategories([]);
+  };
   return <button onClick={onSignOutClick}>Log Out</button>;
 };
 
